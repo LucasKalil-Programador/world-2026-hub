1312d2327a2a7b273d146797259a83be35f3133d
Lucas Kalil <lucas.prokalil2020@outlook.com>
2026-06-17 00:06:02 -0300

    data: update 16/06/2026 00:05 ARGxALG 3x0

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,7 +15,7 @@ import { initStats } from './stats.js';
 
 let data = null;
 
-const DATA_VERSION = '2026-06-16-rev2';
+const DATA_VERSION = '2026-06-16-rev4';
 
 export async function loadData() {
   if (data) return data;
